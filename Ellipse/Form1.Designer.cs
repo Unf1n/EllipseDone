4aba400765f80f67e9947ac280f92bfccc0c986f
Unf1n <118728757+Unf1n@users.noreply.github.com>
2022-12-05 14:35:51 +0500

    1
    Ellipse done

---
+++ Ellipse/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Ellipse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(188, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove_1);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.trackBar1.Location = new System.Drawing.Point(721, 25);
+            this.trackBar1.Maximum = 200;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 500);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.AutoSize = false;
+            this.trackBar2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.trackBar2.Location = new System.Drawing.Point(102, 25);
+            this.trackBar2.Maximum = 180;
+            this.trackBar2.Minimum = -180;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 500);
+            this.trackBar2.SmallChange = 0;
+            this.trackBar2.TabIndex = 3;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            this.trackBar2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBar2_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(890, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.MouseLeave += new System.EventHandler(this.textBox1_MouseLeave);
+            this.textBox1.MouseHover += new System.EventHandler(this.textBox1_MouseHover);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(890, 161);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(239, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(890, 187);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(239, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.MouseLeave += new System.EventHandler(this.textBox3_MouseLeave);
+            this.textBox3.MouseHover += new System.EventHandler(this.textBox3_MouseHover);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Square",
+            "Circle",
+            "Hexagon",
+            "Triangle"});
+            this.comboBox1.Location = new System.Drawing.Point(836, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(239, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "Shape";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(804, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Radius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(804, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Orintation";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(804, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Ratio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1141, 563);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        protected System.Windows.Forms.TrackBar trackBar1;
+        protected System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Ellipse/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Ellipse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.MouseWheel += new MouseEventHandler(this_MouseWheel);
+            SetSize();
+        }
+        private void SetSize()
+        {
+            Rectangle rec = Screen.PrimaryScreen.Bounds;
+            map = new Bitmap(rec.Width, rec.Height);
+            Graf = Graphics.FromImage(map);
+        }
+        Graphics Graf;
+        Bitmap map = new Bitmap(500, 500);
+        double ab;
+        double ty;
+        int rad = 100;
+        float Gr;
+
+        private Matrix RotateAroundPoint(float angle, Point center, bool u)
+        {
+            // Переведите точку в начало.;
+            if (u)
+            {
+                Gr = angle;
+            }
+            else
+                Gr = Gr + angle;
+
+
+            if (Gr > 180)
+            {
+                Gr = -180;
+            }
+            if (Gr < -180)
+            {
+                Gr = 180;
+            }
+            Matrix result = new Matrix();
+            result.RotateAt(Gr, center);
+            return result;
+        }
+
+        int s = 270;
+        float ratio = 1;
+        int tu = 360;
+        public void Polygons(int y, int radius)
+        {
+
+            int n = tu;
+            ab = 360 / n;
+            int f = 60;
+            ty = n / 1;
+            double a = 0;
+            PointF[] curvePoints = new PointF[n + 2];
+            PointF points1 = new PointF(250, 250);
+            curvePoints[n + 1] = points1;
+            for (int i = 0; i <= n; i++)
+            {
+                PointF points = new PointF((float)((pictureBox1.Size.Width / 2) + radius * (Math.Cos((i + s) * ab * Math.PI / 180f) * (ratio))), (float)((pictureBox1.Size.Height / 2) + radius * Math.Sin((i + s) * ab * Math.PI / 180f)));
+                //  MessageBox.Show(points.ToString());
+                a += ab;
+                curvePoints[i] = points;
+            }
+            Graf.FillPolygon(Brushes.Cyan, curvePoints);
+            Graf.DrawPolygon(Pens.Black, curvePoints);
+
+            pictureBox1.Image = map;
+            //MessageBox.Show(" ");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Graf.Clear(Color.Silver);
+            Graf.Transform = RotateAroundPoint(t, center, false);
+            Polygons(t, rad);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Graf.Clear(Color.Silver);
+            for (int i = 0; true; i++)
+            {
+                if (i == 360)
+                {
+                    i = 0;
+                }
+                // Polygons(i,10);
+            }
+        }
+        int asf = 100;
+        bool Mouses;
+        bool MousesD;
+        int kx;
+        int ky;
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            MousesD = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (MousesD)
+                if (kx < e.X)
+                {
+                    Polygons(kx, rad);
+                }
+        }
+        void this_MouseWheel(object sender, MouseEventArgs e)
+        {
+            Graf.Clear(Color.Silver);
+            if (Mouses)
+                if (e.Delta > 0)
+                {
+                    rad++;
+                    if (rad > 0 && rad < 201)
+                    {
+                        Polygons(t, rad);
+                        trackBar1.Value = rad;
+                    }
+                    else
+                        rad = 200;
+                }
+                else
+                {
+                    rad--;
+                    if (rad < 201 && rad > 0)
+                    {
+                        trackBar1.Value = rad;
+                    }
+                    else rad = 1;
+                }
+
+            Polygons(t, rad);
+
+        }
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        int t = 0;
+        int h;
+        Point center = new Point(250, 250);
+        private void pictureBox1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            Mouses = true;
+            if (MousesD)
+            {
+                Graf.Clear(Color.Silver);
+                t = e.X - kx;
+                h = Int32.Parse(Gr.ToString());
+                Graf.Transform = RotateAroundPoint(t, center, false);
+                Polygons(t, rad);
+                trackBar2.Value = h;
+                textBox2.Text = h.ToString();
+            }
+            kx = e.X;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            Mouses = false;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            t = 0;
+            MousesD = false;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+
+            Graf.Clear(Color.Silver);
+            Graf.Transform = RotateAroundPoint(trackBar2.Value, center, true);
+            Polygons(t, rad);
+            textBox2.Text = trackBar2.Value.ToString();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Graf.Clear(Color.Silver);
+            Polygons(t, trackBar1.Value);
+            rad = trackBar1.Value;
+            textBox1.Text = rad.ToString();
+        }
+
+        private void trackBar2_MouseUp(object sender, MouseEventArgs e)
+        {
+            t = 0;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Graf.Transform = RotateAroundPoint(0, center, true);
+            try
+            {
+                Graf.Clear(Color.Silver);
+                Gr = Int32.Parse(textBox2.Text);
+                trackBar2.Value = Int32.Parse(textBox2.Text);
+                Graf.Transform = RotateAroundPoint(Gr, center, true);
+                Polygons(t, rad);
+            }
+            catch (System.FormatException)
+            {
+                if (textBox2.Text != "" && textBox2.Text != "-")
+                    MessageBox.Show("Только цифры", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    Graf.Clear(Color.Silver);
+                    Polygons(t, rad);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Graf.Clear(Color.Silver);
+                rad = Int32.Parse(textBox1.Text);
+                Polygons(t, rad);
+            }
+            catch (System.FormatException)
+            {
+                if (textBox1.Text != "" && textBox1.Text != "-")
+                    MessageBox.Show("Только цифры", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    Graf.Clear(Color.Silver);
+                    Polygons(t, rad);
+                    if (!hovers)
+                        textBox1.Text = "0";
+                }
+            }
+        }
+        bool hovers;
+        private void textBox1_MouseHover(object sender, EventArgs e)
+        {
+            hovers = true;
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            hovers = false;
+            if (textBox1.Text == "")
+                textBox1.Text = "0";
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Polygons(t, rad);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            ratio = 1;
+            try
+            {
+                ratio = Convert.ToSingle(textBox3.Text) / 10;
+                Graf.Clear(Color.Silver);
+                Polygons(t, rad);
+            }
+            catch (System.FormatException)
+            {
+                if (textBox3.Text != "" && textBox3.Text != ".")
+                    MessageBox.Show("Только цифры", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    Graf.Clear(Color.Silver);
+                    Polygons(t, rad);
+                    if (!hovers)
+                        textBox3.Text = "0";
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case ("Hexagon"):
+                    tu = 6;
+                    Graf.Clear(Color.Silver);
+                    Polygons(t, rad);
+                    break;
+                case ("Circle"):
+                    tu = 360;
+                    Graf.Clear(Color.Silver);
+                    Polygons(t, rad);
+                    break;
+                case ("Triangle"):
+                    tu = 3;
+                    Graf.Clear(Color.Silver);
+                    Polygons(t, rad);
+                    break;
+                case ("Square"):
+                    tu = 4;
+                    Graf.Clear(Color.Silver);
+                    Polygons(t, rad);
+                    break;
+            }
+        }
+
+        private void textBox3_MouseHover(object sender, EventArgs e)
+        {
+            hovers = true;
+        }
+
+        private void textBox3_MouseLeave(object sender, EventArgs e)
+        {
+            hovers = false;
+            if (textBox1.Text == "")
+                textBox1.Text = "0";
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            Graf.Clear(Color.Silver);
+            Polygons(t, trackBar1.Value);
+            rad = trackBar1.Value;
+            textBox1.Text = rad.ToString();
+        }
+    }
+}
